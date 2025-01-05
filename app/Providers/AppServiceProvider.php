c72946a0a3619b306b2f8cc7aad23996933dd00d
kuku1198 <kuku1198@gmail.com>
2025-01-05 16:44:57 +0900

    Query File logging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (env('APP_DEBUG')) {
+            DB::listen(function ($query) {
+                Log::channel('database')->info(
+                    "Query Time: {$query->time}ms | SQL: {$query->sql} | Bindings: " . implode(", ", $query->bindings)
+                );
+            });
+        }
     }
 }
